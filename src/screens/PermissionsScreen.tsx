35e321afb4c7c5730fca749264852762a6644e0b
jvid <judara2000@gmail.com>
2022-04-14 09:32:09 -0500

    Change: use permissions context in permissions screen

--- src/screens/PermissionsScreen.tsx
+++ src/screens/PermissionsScreen.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Button, StyleSheet, View} from 'react-native';
+import Text from '../components/ui/Text';
+import {usePermissionsContext} from '../context/PermissionsContext';
 
 const PermissionsScreen = () => {
+  const {permissions, askLocationPermission} = usePermissionsContext();
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>PermissionsScreen</Text>
+      <Button title="Request" onPress={() => askLocationPermission()} />
+      <Text>{JSON.stringify(permissions, null, 3)}</Text>
     </View>
   );
 };
 
 export default PermissionsScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
